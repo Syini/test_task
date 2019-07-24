1404c9ec2ba89665874d4464df4d57c1e955b854
Коваленко Антон Сергійович <anton.kovalenko@fuib.com>
2019-07-24 18:43:28 +0300

    working all filters(in theory)

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/getInfo", urlencodedParser, async function (req, res) {
 
   let response2 = await rp(options1, function (err, response,body) {
   })
-  console.log(response2)
+  //console.log(response2)
 
   res.render('getInfo',{qwe:'qwer',reps: response2, name:req.body.userName, url: helper.html_url, title:'Repositories', repositories_amount: helper.public_repos})
 
@@ -97,16 +97,39 @@ app.get('/getInfo/:userName', urlencodedParser, async function (req, res) {
   Handlebars.registerHelper('card', function(context, options) {
     let ret = ''
     context = JSON.parse(response2)
+    console.log(context.length)
+    if(req.query.issues === 'on') {
+      context = context.filter(item => item.has_issues === true)
+    }
+    // if(req.query.topics === 'on') {
+    //   context = context.filter(item => item.has_issues === true)
+    // }
+    if(req.query.starred === 'on') {
+      context = context.filter(item => item.stargazers_count > req.query.starred_value)
+    }
+    console.log(context.length)
+    // if(req.query.updated === 'on'){
+    //   context = context.filter(item => new Date(item.updated_at) > new Date(req.query.updated_value))
+    // }
+
+    if(req.query.type === 'on'){
+      context = context.filter(item => item.fork === true)
+    }
+    console.log(context.length)
+    if(req.query.language === 'on'){
+      context = context.filter(item => item.language === req.query.language_value)
+    }
+    console.log(context)
     for(let i=0; i<context.length; i++) {
-      if (req.query === {}) {
-        ret += `<a href = ${context[i].svn_url}><div class='card'  >` + "<div class='container'>" + options.fn(context[i]) + "</div>" + "</div></a>"
-      }
-      else {
+
         if (req.query.updated === 'on' && new Date(req.query.updated_value) < new Date(context[i].updated_at)) {
           ret += `<div class='card'  >` + "<div class='container'>" + options.fn(context[i]) + "</div>" + "</div>"
         }
+        else{
+          ret += `<a href = ${context[i].svn_url}><div class='card'  >` + "<div class='container'>" + options.fn(context[i]) + "</div>" + "</div></a>"
+        }
       }
-    }
+
       return ret
   })
 
